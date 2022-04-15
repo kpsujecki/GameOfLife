e6548b37478f63db1a3dc0c14930d7a9321fa0ac
DESKTOP-PHGG8NB\Konra <kondzikxu@gmail.com>
2022-04-15 20:33:16 +0200

    Initial commit

---
+++ View/Board.py
@@ -0,0 +1,16 @@
+import pygame
+import pygame.locals
+
+
+class Board(object):
+    def __init__(self, width, height):
+        self.surface = pygame.display.set_mode((width, height), 0, 32)
+        pygame.display.set_caption("Game of Life", "GoL")
+
+    def draw_board(self, *args):
+        background_board = (0, 0, 0)
+        self.surface.fill(background_board)
+        for drawable in args:
+            drawable.draw_on(self.surface)
+
+        pygame.display.update()
---
+++ main.py
@@ -0,0 +1,13 @@
+from Model.ModelGame import game_of_life
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-he', dest='height', type=int)
+parser.add_argument('-wi', dest='width', type=int)
+
+args = parser.parse_args()
+
+if __name__ == "__main__":
+    game = game_of_life(args.height, args.width)
+    game.run()
